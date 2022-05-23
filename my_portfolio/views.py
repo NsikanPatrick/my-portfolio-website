a31fe951a6a1b5568d5a135a2e4d9292985fd249
NsikanPatrick <nsikanpatrick69@gmail.com>
2022-05-23 13:42:19 +0200

    First commit for demo website

--- my_portfolio/views.py
+++ my_portfolio/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from django.http import FileResponse
+import os
 from django.http import HttpResponse
 
 
 def index(request):
     # return HttpResponse('Halo')
     return render(request, 'index.html')
+
+
+def resume(request):
+    filepath = os.path.join('static', 'Resume.pdf')
+    return FileResponse(open(filepath, 'rb'), content_type='application/pdf')
