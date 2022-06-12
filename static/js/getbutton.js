14fd0c9bb555a342c5ba30291a5bb5c935d67b6e
NsikanPatrick <nsikanpatrick69@gmail.com>
2022-06-12 22:22:26 +0200

    beta commit

---
+++ static/js/getbutton.js
@@ -0,0 +1,14 @@
+<!-- GetButton.io whatsapp chat widget -->
+    (function () {
+        var options = {
+            whatsapp: "+2347069079051", // WhatsApp number
+            call_to_action: "Message us", // Call to action
+            button_color: "#FF6550", // Color of button
+            position: "right", // Position may be 'right' or 'left'
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+<!-- /GetButton.io widget -->
