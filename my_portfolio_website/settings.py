1cd5f67dd62f7b89260346b6b590c1ae485427af
NsikanPatrick <nsikanpatrick69@gmail.com>
2023-09-24 20:49:32 +0200

    Added Resume button

--- my_portfolio_website/settings.py
+++ my_portfolio_website/settings.py
@@ -23,11 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+# Environment variables
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+5ojq6$013ua%9jf9hg3xe$chj4-unhd2rxj(b5vdgddljm0s^'
+# SECRET_KEY = '##################################################'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = #######
 
 ALLOWED_HOSTS = ['*']
 
